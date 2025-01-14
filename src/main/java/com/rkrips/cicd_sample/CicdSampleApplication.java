4e4df72e1d1049c9f9a4a1de92b038c58bfa740b
rkrips <rakeshleo@gmail.com>
2025-01-14 16:42:52 -0500

    First commit

---
+++ src/main/java/com/rkrips/cicd_sample/CicdSampleApplication.java
@@ -0,0 +1,13 @@
+package com.rkrips.cicd_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkrips/cicd_sample/CicdSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkrips.cicd_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
